537d5b174a5ed5e9b79d0be7034595cac4a757ed
Wilde1000 <107768936+Wilde1000@users.noreply.github.com>
2023-03-15 07:16:35 -0400

    Create NHE_Teams.ino

---
+++ NHE_Teams.ino
@@ -0,0 +1,456 @@
+const byte team_5265[][16] PROGMEM = {
+{"G0T100#         "},  //Turns Top FLD to text mode
+{"E52T1#          "},  //Raise Light Saber
+{"B24T1#          "},  //Open Data Panel
+{"B21T7#          "},  //Change Coin Slots to Violet
+{"B22T7#          "},  //Change LDPL to Violet
+{"D40S4#          "},  //Set Color of Holoprojectors to Violet
+{"D45S4#          "},  //Set Color of Magic Panel to Violet
+{"D45T1#          "},  //Turn on Magic Panel
+{"D40T1#          "},  //Turn on Holoprojectors
+{"J0004#          "},  //Play General Sound #4 
+{"G1MTEAM#        "},  //Display TEAM on Top FLD
+{"G2M5265#        "},  //Display 5265 on Bottom FLD
+{"G3MRADICAL#     "},  //Display RADICAL on RLD
+{"W07#            "},  //Wait 7 secs
+{"G3MIMPACT#      "},  //Display IMPACT on RLD
+{"W07#            "},  //Wait 7 secs
+{"E52T2#          "},  //Lower Light Saber
+{"G0T1#           "},  //Return FLD to Normal
+{"B21T16#         "},  //Change coin slots to Blue Single
+{"B22T16#         "},  //Change LDPL to Blue Two Across
+{"B24T2#          "},  //Close the Dataport Door
+{"D40T0#          "},  //Shut off Holoprojectors
+{"D45T0#          "},  //Shut off Magic Panel
+{"D45S5#          "},  //Reset Magic Panel back to Blue
+{"D40S5#          "},  //Reset Holoprojector back to Blue
+{"W03#            "},  //Wait 3 Seconds
+{"@@@@@@#         "}   //End program
+    
+};
+
+
+
+
+const byte team_5902[][16] PROGMEM = {
+{"G0T100#          "},  //Turns Top FLD to text mode
+{"E52T1#           "},  //Raise Light Saber
+{"B24T1#           "},  //Open Data Panel
+{"B21T1#           "},  //Change Coin Slots to Red
+{"B22T1#           "},  //Change LDPL to Red
+{"D40S1#           "},  //Set Color of Holoprojectors to Red
+{"D45S1#           "},  //Set Color of Magic Panel to Red
+{"D45T1#           "},  //Turn on Magic Panel
+{"D40T1#           "},  //Turn on Holoprojectors
+{"J0006#           "},  //Play General Sound #6 
+{"G1MTEAM#         "},  //Display TEAM on Top FLD
+{"G2M5902#         "},  //Display 5902 on Bottom FLD
+{"G3MWIRE#         "},  //Display WIRE on Bottom FLD
+{"W07#             "},  //Wait 7 secs
+{"G3MCLIPPERS#     "},  //Display CLIPPERS on Bottom FLD
+{"W07#             "},  //Wait 7 secs
+{"E52T2#           "},  //Lower Light Saber
+{"G0T1#            "},  //Return FLD to Normal
+{"B21T16#          "},  //Change coin slots to Blue Single
+{"B22T16#          "},  //Change LDPL to Blue Two Across
+{"B24T2#           "},  //Close the Dataport Door
+{"D40T0#           "},  //Shut off Holoprojectors
+{"D45T0#           "},  //Shut off Magic Panel
+{"D45S5#           "},  //Reset Magic Panel back to Blue
+{"D40S5#           "},  //Reset Holoprojector back to Blue
+{"W03#             "},  //Wait 3 Seconds
+{"@@@@@@#          "}   //End program
+    
+};
+
+
+
+
+const byte team_6324[][16] PROGMEM = {
+{"G0T100#          "},  //Turns Top FLD to text mode
+{"E52T1#           "},  //Raise Light Saber
+{"B24T1#           "},  //Open Data Panel
+{"B21T6#           "},  //Change Coin Slots to Blue
+{"B22T6#           "},  //Change LDPL to Blue
+{"D40S5#           "},  //Set Color of Holoprojectors to Blue
+{"D45S5#           "},  //Set Color of Magic Panel to Blue
+{"D45T1#           "},  //Turn on Magic Panel
+{"D40T1#           "},  //Turn on Holoprojectors
+{"J0004#           "},  //Play General Sound #4 
+{"G1MTEAM#         "},  //Display TEAM on Top FLD
+{"G2M6324#         "},  //Display 6324 on Bottom FLD
+{"G3MBLUE DEVILS#  "},  //Display BLUE DEVILS on RLD
+{"W15#             "},  //Wait 15 secs
+{"E52T2#           "},  //Lower Light Saber
+{"G0T1#            "},  //Return FLD to Normal
+{"B21T16#          "},  //Change coin slots to Blue Single
+{"B22T16#          "},  //Change LDPL to Blue Two Across
+{"B24T2#           "},  //Close the Dataport Door
+{"D40T0#           "},  //Shut off Holoprojectors
+{"D45T0#           "},  //Shut off Magic Panel
+{"D45S5#           "},  //Reset Magic Panel back to Blue
+{"D40S5#           "},  //Reset Holoprojector back to Blue
+{"W03#             "},  //Wait 3 Seconds
+{"@@@@@@#          "}   //End program
+    
+};
+
+
+const byte team_6329[][16] PROGMEM = {
+{"G0T100#         "},  //Turns Top FLD to text mode
+{"E52T1#          "},  //Raise Light Saber
+{"B24T1#          "},  //Open Data Panel
+{"B21T7#          "},  //Change Coin Slots to Violet
+{"B22T7#          "},  //Change LDPL to Violet
+{"D40S4#          "},  //Set Color of Holoprojectors to Violet
+{"D45S4#          "},  //Set Color of Magic Panel to Violet
+{"D45T1#          "},  //Turn on Magic Panel
+{"D40T1#          "},  //Turn on Holoprojectors
+{"J0004#          "},  //Play General Sound #4 
+{"G1MTEAM#        "},  //Display TEAM on Top FLD
+{"G2M6329#        "},  //Display 6329 on Bottom FLD
+{"G3MBUCKS WRATH# "},  //Display BUCKS WRATH on RLD
+{"W15#            "},  //Wait 7 secs
+{"E52T2#          "},  //Lower Light Saber
+{"G0T1#           "},  //Return FLD to Normal
+{"B21T16#         "},  //Change coin slots to Blue Single
+{"B22T16#         "},  //Change LDPL to Blue Two Across
+{"B24T2#          "},  //Close the Dataport Door
+{"D40T0#          "},  //Shut off Holoprojectors
+{"D45T0#          "},  //Shut off Magic Panel
+{"D45S5#          "},  //Reset Magic Panel back to Blue
+{"D40S5#          "},  //Reset Holoprojector back to Blue
+{"W03#            "},  //Wait 3 Seconds
+{"@@@@@@#         "}   //End program
+    
+};
+
+
+
+
+const byte team_6367[][16] PROGMEM = {
+{"G0T100#         "},  //Turns Top FLD to text mode
+{"E52T1#          "},  //Raise Light Saber
+{"B24T1#          "},  //Open Data Panel
+{"B21T5#          "},  //Change Coin Slots to Cyan
+{"B22T5#          "},  //Change LDPL to Cyan
+{"D40S7#          "},  //Set Color of Holoprojectors to Cyan
+{"D45S7#          "},  //Set Color of Magic Panel to Cyan
+{"D45T1#          "},  //Turn on Magic Panel
+{"D40T1#          "},  //Turn on Holoprojectors
+{"J0004#          "},  //Play General Sound #4 
+{"G1MTEAM#        "},  //Display TEAM on Top FLD
+{"G2M6367#        "},  //Display 7314 on Bottom FLD
+{"G3MELECTROLIGHT#"},  //Display ELECTROLIGHTS on RLD
+{"W15#            "},  //Wait 15 secs
+{"E52T2#          "},  //Lower Light Saber
+{"G0T1#           "},  //Return FLD to Normal
+{"B21T16#         "},  //Change coin slots to Blue Single
+{"B22T16#         "},  //Change LDPL to Blue Two Across
+{"B24T2#          "},  //Close the Dataport Door
+{"D40T0#          "},  //Shut off Holoprojectors
+{"D45T0#          "},  //Shut off Magic Panel
+{"D45S5#          "},  //Reset Magic Panel back to Blue
+{"D40S5#          "},  //Reset Holoprojector back to Blue
+{"W03#            "},  //Wait 3 Seconds
+{"@@@@@@#         "}   //End program
+    
+};
+
+
+
+
+const byte team_6762[][16] PROGMEM = {
+{"G0T100#          "},  //Turns Top FLD to text mode
+{"E52T1#           "},  //Raise Light Saber
+{"B24T1#           "},  //Open Data Panel
+{"B21T1#           "},  //Change Coin Slots to Red
+{"B22T1#           "},  //Change LDPL to Red
+{"D40S1#           "},  //Set Color of Holoprojectors to Red
+{"D45S1#           "},  //Set Color of Magic Panel to Red
+{"D45T1#           "},  //Turn on Magic Panel
+{"D40T1#           "},  //Turn on Holoprojectors
+{"J0006#           "},  //Play General Sound #6 
+{"G1MTEAM#         "},  //Display TEAM on Top FLD
+{"G2M6762#         "},  //Display 6762 on Bottom FLD
+{"G3MOSCATS#       "},  //Display OSCATS on Bottom FLD
+{"W15#             "},  //Wait 15 secs
+{"E52T2#           "},  //Lower Light Saber
+{"G0T1#            "},  //Return FLD to Normal
+{"B21T16#          "},  //Change coin slots to Blue Single
+{"B22T16#          "},  //Change LDPL to Blue Two Across
+{"B24T2#           "},  //Close the Dataport Door
+{"D40T0#           "},  //Shut off Holoprojectors
+{"D45T0#           "},  //Shut off Magic Panel
+{"D45S5#           "},  //Reset Magic Panel back to Blue
+{"D40S5#           "},  //Reset Holoprojector back to Blue
+{"W03#             "},  //Wait 3 Seconds
+{"@@@@@@#          "}   //End program
+    
+};
+
+
+
+
+const byte team_6895[][16] PROGMEM = {
+{"G0T100#          "},  //Turns Top FLD to text mode
+{"E52T1#           "},  //Raise Light Saber
+{"B24T1#           "},  //Open Data Panel
+{"B21T7#           "},  //Change Coin Slots to Violet
+{"B22T7#           "},  //Change LDPL to Violet
+{"D40S4#           "},  //Set Color of Holoprojectors to Violet
+{"D45S4#           "},  //Set Color of Magic Panel to Violet
+{"D45T1#           "},  //Turn on Magic Panel
+{"D40T1#           "},  //Turn on Holoprojectors
+{"J0004#           "},  //Play General Sound #4 
+{"G1MTEAM#         "},  //Display TEAM on Top FLD
+{"G2M6895#         "},  //Display 6895 on Bottom FLD
+{"G3MMECHANICAL#   "},  //Display MECHANICAL on RLD
+{"W07#             "},  //Wait 7 secs
+{"G3MMADNESS 4H#   "},  //Display MADNESS on RLD
+{"W07#             "},  //Wait 7 secs
+{"E52T2#           "},  //Lower Light Saber
+{"G0T1#            "},  //Return FLD to Normal
+{"B21T16#          "},  //Change coin slots to Blue Single
+{"B22T16#          "},  //Change LDPL to Blue Two Across
+{"B24T2#           "},  //Close the Dataport Door
+{"D40T0#           "},  //Shut off Holoprojectors
+{"D45T0#           "},  //Shut off Magic Panel
+{"D45S5#           "},  //Reset Magic Panel back to Blue
+{"D40S5#           "},  //Reset Holoprojector back to Blue
+{"W03#             "},  //Wait 3 Seconds
+{"@@@@@@#          "}   //End program
+    
+};
+
+
+const byte team_7314[][16] PROGMEM = {
+{"G0T100#          "},  //Turns Top FLD to text mode
+{"E52T1#           "},  //Raise Light Saber
+{"B24T1#           "},  //Open Data Panel
+{"B21T6#           "},  //Change Coin Slots to Blue
+{"B22T6#           "},  //Change LDPL to Blue
+{"D40S5#           "},  //Set Color of Holoprojectors to Blue
+{"D45S5#           "},  //Set Color of Magic Panel to Blue
+{"D45T1#           "},  //Turn on Magic Panel
+{"D40T1#           "},  //Turn on Holoprojectors
+{"J0004#           "},  //Play General Sound #4 
+{"G1MTEAM#         "},  //Display TEAM on Top FLD
+{"G2M7314#         "},  //Display 7314 on Bottom FLD
+{"G3MTORNADOES#    "},  //Display TORNADOES on RLD
+{"W15#             "},  //Wait 15 secs
+{"E52T2#           "},  //Lower Light Saber
+{"G0T1#            "},  //Return FLD to Normal
+{"B21T16#          "},  //Change coin slots to Blue Single
+{"B22T16#          "},  //Change LDPL to Blue Two Across
+{"B24T2#           "},  //Close the Dataport Door
+{"D40T0#           "},  //Shut off Holoprojectors
+{"D45T0#           "},  //Shut off Magic Panel
+{"D45S5#           "},  //Reset Magic Panel back to Blue
+{"D40S5#           "},  //Reset Holoprojector back to Blue
+{"W03#             "},  //Wait 3 Seconds
+{"@@@@@@#          "}   //End program
+    
+};
+
+
+const byte team_7674[][16] PROGMEM = {
+{"G0T100#          "},  //Turns Top FLD to text mode
+{"E52T1#           "},  //Raise Light Saber
+{"B24T1#           "},  //Open Data Panel
+{"B21T1#           "},  //Change Coin Slots to Red
+{"B22T1#           "},  //Change LDPL to Red
+{"D40S1#           "},  //Set Color of Holoprojectors to Red
+{"D45S1#           "},  //Set Color of Magic Panel to Red
+{"D45T1#           "},  //Turn on Magic Panel
+{"D40T1#           "},  //Turn on Holoprojectors
+{"J0006#           "},  //Play General Sound #6 
+{"G1MTEAM#         "},  //Display TEAM on Top FLD
+{"G2M7674#         "},  //Display 7674 on Bottom FLD
+{"G3MRAIDERBOTS#   "},  //Display RAIDERBOTS on Bottom FLD
+{"W15#             "},  //Wait 15 secs
+{"E52T2#           "},  //Lower Light Saber
+{"G0T1#            "},  //Return FLD to Normal
+{"B21T16#          "},  //Change coin slots to Blue Single
+{"B22T16#          "},  //Change LDPL to Blue Two Across
+{"B24T2#           "},  //Close the Dataport Door
+{"D40T0#           "},  //Shut off Holoprojectors
+{"D45T0#           "},  //Shut off Magic Panel
+{"D45S5#           "},  //Reset Magic Panel back to Blue
+{"D40S5#           "},  //Reset Holoprojector back to Blue
+{"W03#             "},  //Wait 3 Seconds
+{"@@@@@@#          "}   //End program
+    
+};
+
+
+const byte team_7913[][16] PROGMEM = {
+{"G0T100#          "},  //Turns Top FLD to text mode
+{"E52T1#           "},  //Raise Light Saber
+{"B24T1#           "},  //Open Data Panel
+{"B21T4#           "},  //Change Coin Slots to Green
+{"B22T4#           "},  //Change LDPL to Green
+{"D40S9#           "},  //Set Color of Holoprojectors to Green
+{"D45S9#           "},  //Set Color of Magic Panel to Green
+{"D45T1#           "},  //Turn on Magic Panel
+{"D40T1#           "},  //Turn on Holoprojectors
+{"J0007#           "},  //Play General Sound #7 
+{"G1MTEAM#         "},  //Display TEAM on Top FLD
+{"G2M7913#         "},  //Display 7913 on Bottom FLD
+{"G3MBEAR LY #     "},  //Display BEAR LY on RLD
+{"W07#             "},  //Wait 7 secs
+{"G3MFUNCTIONING#  "},  //Display FUNCTIONING on RLD
+{"W07#             "},  //Wait 7 secs
+{"E52T2#           "},  //Lower Light Saber
+{"G0T1#            "},  //Return FLD to Normal
+{"B21T16#          "},  //Change coin slots to Blue Single
+{"B22T16#          "},  //Change LDPL to Blue Two Across
+{"B24T2#           "},  //Close the Dataport Door
+{"D40T0#           "},  //Shut off Holoprojectors
+{"D45T0#           "},  //Shut off Magic Panel
+{"D45S5#           "},  //Reset Magic Panel back to Blue
+{"D40S5#           "},  //Reset Holoprojector back to Blue
+{"W03#             "},  //Wait 3 Seconds
+{"@@@@@@#          "}   //End program
+    
+};
+
+
+
+const byte team_8046[][16] PROGMEM = {
+{"G0T100#          "},  //Turns Top FLD to text mode
+{"E52T1#           "},  //Raise Light Saber
+{"B24T1#           "},  //Open Data Panel
+{"B21T6#           "},  //Change Coin Slots to Blue
+{"B22T6#           "},  //Change LDPL to Blue
+{"D40S5#           "},  //Set Color of Holoprojectors to Blue
+{"D45S5#           "},  //Set Color of Magic Panel to Blue
+{"D45T1#           "},  //Turn on Magic Panel
+{"D40T1#           "},  //Turn on Holoprojectors
+{"J0004#           "},  //Play General Sound #4 
+{"G1MTEAM#         "},  //Display TEAM on Top FLD
+{"G2M8410#         "},  //Display 8046 on Bottom FLD
+{"G3MLAKERBOTS#    "},  //Display LAKERBOTS on RLD
+{"W15#             "},  //Wait 15 secs
+{"E52T2#           "},  //Lower Light Saber
+{"G0T1#            "},  //Return FLD to Normal
+{"B21T16#          "},  //Change coin slots to Blue Single
+{"B22T16#          "},  //Change LDPL to Blue Two Across
+{"B24T2#           "},  //Close the Dataport Door
+{"D40T0#           "},  //Shut off Holoprojectors
+{"D45T0#           "},  //Shut off Magic Panel
+{"D45S5#           "},  //Reset Magic Panel back to Blue
+{"D40S5#           "},  //Reset Holoprojector back to Blue
+{"W03#             "},  //Wait 3 Seconds
+{"@@@@@@#          "}   //End program
+    
+};
+
+
+const byte team_8410[][16] PROGMEM = {
+{"G0T100#          "},  //Turns Top FLD to text mode
+{"E52T1#           "},  //Raise Light Saber
+{"B24T1#           "},  //Open Data Panel
+{"B21T6#           "},  //Change Coin Slots to Blue
+{"B22T6#           "},  //Change LDPL to Blue
+{"D40S5#           "},  //Set Color of Holoprojectors to Blue
+{"D45S5#           "},  //Set Color of Magic Panel to Blue
+{"D45T1#           "},  //Turn on Magic Panel
+{"D40T1#           "},  //Turn on Holoprojectors
+{"J0002#           "},  //Play General Sound #2 
+{"G1MTEAM#         "},  //Display TEAM on Top FLD
+{"G2M8410#         "},  //Display 8410 on Bottom FLD
+{"G3MOYSTER RIVER# "},  //Display OYSTER RIVER on RLD
+{"W07#             "},  //Wait 15 secs
+{"G3MOVERDRIVE#    "},  //Display OVERDRIVE on RLD
+{"W07#             "},  //Wait 15 secs
+{"E52T2#           "},  //Lower Light Saber
+{"G0T1#            "},  //Return FLD to Normal
+{"B21T16#          "},  //Change coin slots to Blue Single
+{"B22T16#          "},  //Change LDPL to Blue Two Across
+{"B24T2#           "},  //Close the Dataport Door
+{"D40T0#           "},  //Shut off Holoprojectors
+{"D45T0#           "},  //Shut off Magic Panel
+{"D45S5#           "},  //Reset Magic Panel back to Blue
+{"D40S5#           "},  //Reset Holoprojector back to Blue
+{"W03#             "},  //Wait 3 Seconds
+{"@@@@@@#          "}   //End program
+    
+};
+
+
+
+const byte team_9055[][16] PROGMEM = {
+{"G0T100#          "},
+{"E54T1#           "},
+{"B24T1#           "},
+{"B21T1#           "},
+{"B22T1#           "},
+{"D40S1#           "},
+{"D45S1#           "},
+{"D45T1#           "},
+{"D40T1#           "},
+{"JS#              "},
+{"G1MTEAM#         "},
+{"G2M9055#         "},
+{"G3MKNIGHTS OF #  "},
+{"W07#             "},
+{"G3MKATADIN#      "},
+{"W07#             "},
+{"E54T2#           "},
+{"G0T1#            "},
+{"B21T16#          "},
+{"B22T16#          "},
+{"B24T2#           "},
+{"D40T0#           "},
+{"D45T0#           "},
+{"D45S5#           "},
+{"D40S5#           "},
+{"W03#             "},
+{"@@@@@@#          "}  
+};
+
+const byte team_9101[][16] PROGMEM = {
+{"G0T100#          "},  //Turns FLD and RLD to text mode
+{"E54T1#           "},  //Raise Motivator
+{"B24T1#           "},  //Open Data Panel
+{"B21T4#           "},  //Change Coin Slots to Green
+{"B22T4#           "},  //Change LDPL to Red
+{"D40S9#           "},  //Set Color of Holoprojectors to Green
+{"D45S9#           "},  //Set Color of Magic Panel to Green
+{"D45T1#           "},  //Turn on Magic Panel
+{"D40T1#           "},  //Turn on Holoprojectors
+{"J0002#           "},  //Play General Sound #2 
+{"G1MTEAM#         "},  //Display TEAM on Top FLD
+{"G2M9101#         "},  //Display 9101 on Bottom FLD
+{"G3MGREEN MTN    #"},  //Display GREEN MTN on RLD
+{"W07#             "},  //Wait 7 secs
+{"G3MROBOTICS#     "},  //Display ROBOTICS on RLD
+{"W07#             "},  //Wait 7 secs
+{"E54T2#           "},  //Lower Motivator
+{"G0T1#            "},  //Return FLD to Normal
+{"B21T16#          "},  //Change coin slots to Blue Single
+{"B22T16#          "},  //Change LDPL to Blue Two Across
+{"B24T2#           "},  //Close the Dataport Door
+{"D40T0#           "},  //Shut off Holoprojectors
+{"D45T0#           "},  //Shut off Magic Panel
+{"D45S5#           "},  //Reset Magic Panel back to Blue
+{"D40S5#           "},  //Reset Holoprojector back to Blue
+{"W03#             "},  //Wait 3 Seconds
+{"@@@@@@#          "}   //End program
+    
+};
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
