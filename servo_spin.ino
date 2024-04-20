643fb960e6c7bd9e3910a701f1ee77e63cb7d8f4
Wilde1000 <107768936+Wilde1000@users.noreply.github.com>
2024-04-20 10:36:07 -0400

    Update servo_spin.ino

--- servo_spin.ino
+++ servo_spin.ino
@@ -1,40 +1,39 @@
-const int stepPin1 = 2;
+const int stepPin1 = 4;
 const int dirPin1 = 3;
-const int stepPin2 = 4;
+const int stepPin2 = 6;
 const int dirPin2 = 5;
-const int button = 6;
-const int lservo1 = 7;
-const int lservo2 = 8;
+const int button = 7;
+const int lservo1 = 8;
+const int lservo2 = 9;
 
 const int stepsPerRev=200;
-int numofSteps = 3100;
+int numofSteps = 3000;
 int pulseWidthMicros = 100; 	// microseconds
 int millisBtwnSteps = 100;
+int liftState=LOW;
 
-void Pololu_A4988_Example()
+void moveArm(int direct)
 {  
-  delay(500);   // delay for Controller startup
-  digitalWrite(dirPin, LOW);     // Turn left
+  delay(50);   // delay for Controller startup
+  if(direct){
+    digitalWrite(dirPin1, LOW);     // Turn left
+    digitalWrite(dirPin2, LOW);
+  }else {
+    digitalWrite(dirPin1, HIGH);     // Turn left
+    digitalWrite(dirPin2, HIGH);
+  }
   int delayUs = 1000;
   for(double i = 0; i < numofSteps; i++)  //5000 steps in one direction
   {   
-    digitalWrite(stepPin, HIGH);
+    digitalWrite(stepPin1, HIGH);
+    digitalWrite(stepPin2, HIGH);
     delayMicroseconds(delayUs);
-    digitalWrite(stepPin, LOW);
+    digitalWrite(stepPin1, LOW);
+    digitalWrite(stepPin2, LOW);
     delayMicroseconds(delayUs);
     if(delayUs > 300) delayUs-=5;
   } 
-  /*delay(500);   // delay for Controller startup
-  digitalWrite(9, LOW);     // Turn left
-  for(double i = 0; i < 5000; i++)  //5000 steps in one direction
-  {   
-    digitalWrite(10, HIGH);
-    delay(1);
-    digitalWrite(10, LOW);
-    delay(1);
-  } 
-  delay(1000);
-  */
+  return;
 } 
 void setup() {
  	Serial.begin(9600);
@@ -43,12 +42,13 @@ void setup() {
  	pinMode(dirPin1, OUTPUT);
   pinMode(dirPin2, OUTPUT);
   pinMode(button, INPUT_PULLUP);
- 	Serial.println(F("A4988 Initialized"));
-
-  
-
+ 
 }
 
 void loop() {
- 
+ if(digitalRead(button)==0){
+    liftState=!liftState;
+    moveArm(liftState);
+    delay(50);
+ } 
 }
